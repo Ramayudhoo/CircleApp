9cbb8c474a3c8f7be2f2502fd3e177274a95a147
ramayudhoo <yudoyono67@gmail.com>
2026-08-07 13:28:31 +0700

    fix: socket.io

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
-  autoConnect: true,
-});
+const socket = io(
+  import.meta.env.VITE_API_URL_BASE || "http://localhost:3000",
+  {
+    autoConnect: true,
+  },
+);
 
 export default socket;
